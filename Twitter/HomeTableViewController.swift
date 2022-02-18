62f15624e7025029d490ac33db5997eba581299e
Carlos Chavez <cenric09@gmail.com>
2022-02-18 17:17:07 -0500

    Initial Commit

---
+++ Twitter/HomeTableViewController.swift
@@ -0,0 +1,98 @@
+//
+//  HomeTableViewController.swift
+//  Twitter
+//
+//  Created by Carlos Chavez on 2/17/22.
+//  Copyright © 2022 Dan. All rights reserved.
+//
+
+import UIKit
+
+class HomeTableViewController: UITableViewController {
+    
+    var tweetArr = [NSDictionary]()
+    var numberOfTweets: Int!
+    
+    let myRefreshControl = UIRefreshControl()
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print(self.tweetArr)
+        loadTweet()
+
+        myRefreshControl.addTarget(self, action: #selector(loadTweet), for: .valueChanged)
+        tableView.refreshControl = myRefreshControl
+    }
+    
+    
+    
+    @objc func loadTweet() {
+        let myUrl = "https://api.twitter.com/1.1/statuses/home_timeline.json"
+        let myParams = ["count": 10]
+        
+        TwitterAPICaller.client?.getDictionariesRequest(url: myUrl, parameters: myParams, success: { (tweets: [NSDictionary]) in
+            
+            self.tweetArr.removeAll()
+            
+            for tweet in tweets {
+                self.tweetArr.append(tweet)
+            }
+            
+            self.tableView.reloadData()
+            
+            self.myRefreshControl.endRefreshing()
+            
+        }, failure: { error in
+            print(error.localizedDescription)
+        })
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return tweetArr.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "tweetCell", for: indexPath) as! TweetTableViewCell
+        
+        let user = tweetArr[indexPath.row]["user"] as! NSDictionary
+        
+        
+        cell.username.text = user["name"] as? String
+        cell.tweetContent.text = tweetArr[indexPath.row]["text"] as? String
+        
+        let imageUrl = URL(string: (user["profile_image_url_https"] as? String)!)
+        let data = try? Data(contentsOf: imageUrl!)
+
+        if let imageData = data {
+            cell.profileImage.image = UIImage(data: imageData)
+        }
+        
+        return cell
+    }
+    
+    @IBAction func onLogout(_ sender: Any) {
+        TwitterAPICaller.client?.logout()
+        self.dismiss(animated: true, completion: nil)
+        UserDefaults.standard.set(false, forKey: "userLoggedIn")
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
